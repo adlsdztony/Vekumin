af8c98caf5dcbb89f527c4771a3d0523621d34d3
Uynaity <u3609460@connect.hku.hk>
2024-10-06 14:30:24 +0800

    Add TopBar at AuthActivity Improve Permission Application Method Update Version

--- app/src/main/java/icu/hku/vekumin/alarm/AlarmSetter.kt
+++ app/src/main/java/icu/hku/vekumin/alarm/AlarmSetter.kt
@@ -2,13 +2,14 @@ package icu.hku.vekumin.alarm
 
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import androidx.core.app.AlarmManagerCompat.canScheduleExactAlarms
+import androidx.core.content.ContextCompat.startActivity
 import icu.hku.vekumin.alarm.data.AlarmConfig
 import java.util.Calendar
-
 
 class AlarmSetter {
     fun setAlarm(context: Context, alarmConfig: AlarmConfig, aheadDays: Int = 0) {
@@ -16,14 +17,29 @@ class AlarmSetter {
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         if (!canScheduleExactAlarms(alarmManager)) {
-            Toast.makeText(context, "Cannot schedule exact alarms", Toast.LENGTH_SHORT).show()
-            val intent = Intent(android.provider.Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM)
-            context.startActivity(intent)
+            val intent = Intent().apply {
+                setClassName(
+                    "com.android.settings",
+                    "com.android.settings.Settings\$AlarmsAndRemindersActivity"
+                )
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            try {
+                startActivity(context, intent, null)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+                // Handle the failure to open the settings page
+                Toast.makeText(
+                    context, "Unable to open alarm and reminder settings page", Toast.LENGTH_SHORT
+                ).show()
+            }
             return
         }
 
         val intent = Intent(context, AlarmReceiver::class.java).apply {
-            putExtra("alarmConfig", alarmConfig.toConfigString()) // 将目标时间点传递给广播接收器
+            putExtra(
+                "alarmConfig", alarmConfig.toConfigString()
+            ) // Pass the target time to the broadcast receiver
         }
         val pendingIntent = PendingIntent.getBroadcast(
             context,
@@ -32,13 +48,11 @@ class AlarmSetter {
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
-
         val calendar = Calendar.getInstance().apply {
             set(Calendar.HOUR_OF_DAY, getHour(targetTime))
             set(Calendar.MINUTE, getMinute(targetTime))
             set(Calendar.SECOND, 0)
         }
-
 
         println("Alarm ${alarmConfig.id} set at: ${calendar.time}")
 
@@ -46,15 +60,11 @@ class AlarmSetter {
 
         try {
             alarmManager.setExactAndAllowWhileIdle(
-                AlarmManager.RTC_WAKEUP,
-                triggerAtMillis,
-                pendingIntent
+                AlarmManager.RTC_WAKEUP, triggerAtMillis, pendingIntent
             )
         } catch (e: SecurityException) {
             Toast.makeText(
-                context,
-                "Cannot schedule exact alarms: ${e.message}",
-                Toast.LENGTH_SHORT
+                context, "Cannot schedule exact alarms: ${e.message}", Toast.LENGTH_SHORT
             ).show()
         }
     }
