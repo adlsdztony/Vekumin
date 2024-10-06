baf19e55fa6f0eee634665669ace09648c00db35
adlsdztony <zzl0712@outlook.com>
2024-10-06 17:48:45 +0800

    use createPoster method in Secret instead of PosterFactory

--- app/src/main/java/icu/hku/vekumin/post/Platform.kt
+++ app/src/main/java/icu/hku/vekumin/post/Platform.kt
@@ -1,14 +1,25 @@
 package icu.hku.vekumin.post
 
 import android.net.Uri
+import icu.hku.vekumin.post.data.Secret
+import icu.hku.vekumin.post.postables.FakePoster
+import icu.hku.vekumin.post.postables.ThreadPoster
+import icu.hku.vekumin.post.postables.XPoster
 
 enum class Platform {
     THREAD, X, FAKE;
 
     val authUrl: Uri
         get() = when (this) {
-            Platform.THREAD -> Uri.parse("https://thread.icu.hku.icu/auth")
-            Platform.X -> Uri.parse("https://x.icu.hku.icu/auth")
-            Platform.FAKE -> Uri.parse("https://fake.icu.hku.icu/auth")
+            THREAD -> Uri.parse("https://thread.icu.hku.icu/auth")
+            X -> Uri.parse("https://x.icu.hku.icu/auth")
+            FAKE -> Uri.parse("https://fake.icu.hku.icu/auth")
+        }
+
+    val createPoster: (Secret) -> Postable
+        get() = when (this) {
+            THREAD -> { secret -> ThreadPoster(secret) }
+            X -> { secret -> XPoster(secret) }
+            FAKE -> { secret -> FakePoster(secret) }
         }
 }
