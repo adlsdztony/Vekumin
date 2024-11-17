de8129fe3e0b9075d50311e8c4e0245543eb2311
adlsdztony <zzl0712@outlook.com>
2024-11-17 20:42:40 +0800

    fix: post editor

--- app/src/main/java/icu/hku/vekumin/PostActivity.kt
+++ app/src/main/java/icu/hku/vekumin/PostActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
